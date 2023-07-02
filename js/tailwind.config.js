f536c375460e8c9d571c47de4d9fdd697b5b3d6b
Raphael Anizio <raphaelanizio01@gmail.com>
2023-07-02 12:49:00 -0300

    projeto fintech

---
+++ js/tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        primary: "#2A867D",
+      },
+      fontFamily: {
+        twinkle: ['"Twinkle Star"', "cursive"],
+        sans: [
+          '"Ubuntu"',
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
+  },
+};
